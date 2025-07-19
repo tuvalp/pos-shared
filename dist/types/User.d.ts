e508ba0abe9f0cf9f9f6f4069cc44937c642c381
Mac Computer <maccomputer@Macs-MacBook-Pro.local>
2025-07-19 14:20:10 +0300

    build

--- dist/types/User.d.ts
+++ dist/types/User.d.ts
@@ -1,17 +1,17 @@
 import { Document, Types } from "mongoose";
 import { Role } from "./Role";
 export interface User {
-    _id?: Types.ObjectId;
+    id?: Types.ObjectId;
     username: string;
     password: string;
     role: Role;
     color: string;
 }
 export interface UserDocument extends User, Document {
-    _id: Types.ObjectId;
+    id: Types.ObjectId;
 }
 export declare const UserModel: import("mongoose").Model<UserDocument, {}, {}, {}, Document<unknown, {}, UserDocument, {}> & UserDocument & Required<{
-    _id: Types.ObjectId;
+    _id: unknown;
 }> & {
     __v: number;
 }, any>;
